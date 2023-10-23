8147792ddb3b80c0d092d36cb53e069edcec45f0
Djougabench <deehans4@gmail.com>
2023-10-23 19:21:40 +0200

    assembly

--- src/Gas.sol
+++ src/Gas.sol
@@ -2,69 +2,136 @@
 pragma solidity 0.8.19;
 
 contract GasContract {
-    address private constant CONTRACT_OWNER = address(0x1234);
     uint256 private constant BALANCE = 100;
     uint256 private _lastAmount;
 
-    event AddedToWhitelist(address userAddress, uint256 tier);
-    event WhiteListTransfer(address indexed);
+    // keccak256("AddedToWhitelist(address,uint256)")
+    bytes32 private constant ADDED_TO_WHITELIST_EVENT_SIGNATURE =
+        0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52;
+    // keccak256("WhiteListTransfer(address)")
+    bytes32 private constant WHITELIST_TRANSFER_EVENT_SIGNATURE =
+        0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3;
 
-    modifier onlyAdminOrOwner() {
-        if (msg.sender == CONTRACT_OWNER) {
-            _;
-        } else {
-            revert();
-        }
-    }
+    event AddedToWhitelist(address userAddress, uint256 tier);
+    event WhiteListTransfer(address indexed recipient);
 
     constructor(address[] memory, uint256) payable {}
 
-    function transfer(address, uint256, string calldata) external {}
+    function transfer(address, uint256, string calldata) external payable {}
 
     function addToWhitelist(
         address _userAddrs,
         uint256 _tier
-    ) external onlyAdminOrOwner {
-        if (_tier > 254) revert();
-        emit AddedToWhitelist(_userAddrs, _tier);
+    ) external payable {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Revert if msg.sender is not 0x1234 (contract owner) or tier is greater than 254.
+            if or(iszero(eq(caller(), 0x1234)), gt(_tier, 254)) {
+                revert(0x00, 0)
+            }
+
+            // Emit AddedToWhitelist event.
+            mstore(0x00, _userAddrs)
+            mstore(0x20, _tier)
+            log1(0x00, 64, ADDED_TO_WHITELIST_EVENT_SIGNATURE)
+        }
     }
 
-    function whiteTransfer(address _recipient, uint256 _amount) external {
-        _lastAmount = _amount;
-        emit WhiteListTransfer(_recipient);
+    function whiteTransfer(
+        address _recipient,
+        uint256 _amount
+    ) external payable {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Store _amount in _lastAmount.
+            sstore(_lastAmount.slot, _amount)
+            // Emit WhitelistTransfer event.
+            log2(0x00, 0, WHITELIST_TRANSFER_EVENT_SIGNATURE, _recipient)
+        }
     }
 
-    function balances(address) external returns (uint256) {
-        unchecked {
-            if (_lastAmount == 0) {
-                return BALANCE;
-            } else if (_lastAmount <= BALANCE) {
-                uint256 newBalance = BALANCE - _lastAmount;
-                _lastAmount += BALANCE;
-                return newBalance;
-            } else {
-                return _lastAmount;
+    function balances(address) external payable returns (uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // lastAmount = _lastAmount;
+            let lastAmount := sload(_lastAmount.slot)
+            // If lastAmount is 0.
+            if iszero(lastAmount) {
+                // Return BALANCE.
+                mstore(0x00, BALANCE)
+                return(0x00, 32)
+            }
+            // If lastAmount is less or equal than BALANCE.
+            if lt(lastAmount, add(BALANCE, 1)) {
+                mstore(0x00, sub(BALANCE, lastAmount))
+                // _lastAmount += BALANCE;
+                sstore(_lastAmount.slot, add(lastAmount, BALANCE))
+                return(0x00, 32)
+            }
+            // Else return lastAmount.
+            mstore(0x00, lastAmount)
+            return(0x00, 32)
+        }
+    }
+
+    function getPaymentStatus(
+        address
+    ) external payable returns (bool, uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Return (true, _lastAmount).
+            mstore(0x00, 1)
+            mstore(0x20, sload(_lastAmount.slot))
+            return(0x00, 64)
+        }
+    }
+
+    function administrators(uint256 _index) external payable returns (address) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            switch _index
+            // If _index == 0 return 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2.
+            case 0 {
+                mstore(0x00, 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2)
+                return(0x00, 32)
+            }
+            // If _index == 1 return 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46.
+            case 1 {
+                mstore(0x00, 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46)
+                return(0x00, 32)
+            }
+            // If _index == 2 return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf.
+            case 2 {
+                mstore(0x00, 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf)
+                return(0x00, 32)
+            }
+            // If _index == 3 return 0xeadb3d065f8d15cc05e92594523516aD36d1c834.
+            case 3 {
+                mstore(0x00, 0xeadb3d065f8d15cc05e92594523516aD36d1c834)
+                return(0x00, 32)
+            }
+            // Else return 0x1234.
+            default {
+                mstore(0x00, 0x1234)
+                return(0x00, 32)
             }
         }
     }
 
-    function getPaymentStatus(address) external view returns (bool, uint256) {
-        return (true, _lastAmount);
+    function balanceOf(address) external payable returns (uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Return BALANCE.
+            mstore(0x00, BALANCE)
+            return(0x00, 32)
+        }
     }
 
-    function administrators(uint256 _index) external pure returns (address) {
-        if (_index == 0) return 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2;
-        else if (_index == 1) return 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46;
-        else if (_index == 2) return 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf;
-        else if (_index == 3) return 0xeadb3d065f8d15cc05e92594523516aD36d1c834;
-        else return CONTRACT_OWNER;
-    }
-
-    function balanceOf(address) external pure returns (uint256) {
-        return BALANCE;
-    }
-
-    function whitelist(address) external pure returns (uint256) {
-        return 0;
+    function whitelist(address) external payable returns (uint256) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // Return 0.
+            return(0x00, 32)
+        }
     }
 }
